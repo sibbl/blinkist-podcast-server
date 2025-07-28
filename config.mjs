4f0b536c0a01e57a2768d32030942283613d3834
Sebastian Schubotz <github@sibbl.net>
2025-07-28 11:10:50 +0200

    docs: document episodesToKeep

--- config.mjs
+++ config.mjs
@@ -1,5 +1,10 @@
 export default {
   languages: ["de", "en"],
+  // Uncomment the block below to limit how many episodes are kept per locale
+  // episodesToKeep: {
+  //   de: 30, // keep the latest 30 German episodes
+  //   en: 20  // keep the latest 20 English episodes
+  // },
   episodesToKeep: {},
   headless: true,
   scrapeParallel: false,
